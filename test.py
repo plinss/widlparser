da1333d7c4c3133a25c489f24cab968a1ec93348
Tab Atkins-Bittner <jackalmage@gmail.com>
2022-05-26 15:02:27 -0700

    Shorten the casts.

--- test.py
+++ test.py
@@ -17,6 +17,7 @@ import sys
 from typing import cast
 
 import widlparser
+from widlparser import Construct
 
 
 def debug_hook(type, value, tb):
@@ -364,15 +365,15 @@ interface BigNumbers {
             print('    ' + member.idl_type + ': ' + str(member.normal_name) + ' (' + str(member.name) + ')')
 
     print("FIND:")
-    print(cast(widlparser.Construct, parser.find('round')).full_name)
-    print(cast(widlparser.Construct, parser.find('Foo/method/y')).full_name)
-    print(cast(widlparser.Construct, parser.find('Foo.method')).full_name)
-    print(cast(widlparser.Construct, parser.find('Foo(constructor)')).full_name)
-    print(cast(widlparser.Construct, parser.find('longest')).full_name)
-    print(cast(widlparser.Construct, parser.find('fooArg')).full_name)
-    print(cast(widlparser.Construct, parser.find('Window')).full_name)
-    print(cast(widlparser.Construct, parser.find('mediaText')).full_name)
-    print(cast(widlparser.Construct, parser.find('Foo.method')).markup(Marker()))
+    print(cast(Construct, parser.find('round')).full_name)
+    print(cast(Construct, parser.find('Foo/method/y')).full_name)
+    print(cast(Construct, parser.find('Foo.method')).full_name)
+    print(cast(Construct, parser.find('Foo(constructor)')).full_name)
+    print(cast(Construct, parser.find('longest')).full_name)
+    print(cast(Construct, parser.find('fooArg')).full_name)
+    print(cast(Construct, parser.find('Window')).full_name)
+    print(cast(Construct, parser.find('mediaText')).full_name)
+    print(cast(Construct, parser.find('Foo.method')).markup(Marker()))
     for method in parser.find_all('Foo.method'):
         print(method.full_name)
 
